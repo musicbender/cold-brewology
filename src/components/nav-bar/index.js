f6d44988f897da8fab13acff370f06335454435d
musicbender <musicbender@gmail.com>
2017-12-16 11:07:43 -0800

    added and init styled navbar

--- src/components/nav-bar/index.js
+++ src/components/nav-bar/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './nav-bar.scss';
 
 export default (props) => {
   return (
      <div className="nav-bar">
       <ul className="nav-links">
-        <li><NavLink to="/" /></li>
-        <li><NavLink to="/brewers" /></li>
-        <li><NavLink to="/books" /></li>
-        <li><NavLink to="/accessories" /></li>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/brewers">Brewers</NavLink></li>
+        <li><NavLink to="/books">Books</NavLink></li>
+        <li><NavLink to="/accessories">Accessories</NavLink></li>
+        <li><NavLink to="/">Contact</NavLink></li>
       </ul>
      </div>
   );
