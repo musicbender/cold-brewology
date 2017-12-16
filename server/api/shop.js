a31df1742b5c993d16c0bbcc37ec407e0963ebed
musicbender <musicbender@gmail.com>
2017-12-16 13:55:40 -0800

    working item lookup oporations with multiple items with amazon api and custom endpoint

--- server/api/shop.js
+++ server/api/shop.js
@@ -2,22 +2,26 @@ import express from 'express';
 import path from 'path';
 import { apiResponse } from '../util';
 import { OperationHelper } from 'apac';
+import products from '../data/products';
 import config from '../config';
 
 const router = express.Router();
 const APAC = new OperationHelper(config.APAC);
 
 router.get('/get-shop/:type/:page', (req, res) => {
-  APAC.execute('ItemLookup', {
-    idType: 'ASIN',
-    itemId: 'B0001GSSIO',
-  })
-  .then((data) => {
-    apiResponse(res, 200, 0, 'success', data);
-  })
-  .catch((err) => {
-    apiResponse(res, 200, 0, 'error', err);
-  })
+    APAC.execute('ItemLookup', {
+      ItemId: products[req.params.type],
+      idType: 'ASIN',
+    })
+    .then((data) => {
+      apiResponse(res, 200, 0, 'success', data);
+    })
+    .catch((err) => {
+      apiResponse(res, 500, 0, 'error', err);
+    })
+
+
+
 });
 
 export default router;
