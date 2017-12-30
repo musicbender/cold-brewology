2d10e7d3e542056453191f3d72e938ce85e537b3
musicbender <musicbender@gmail.com>
2017-12-29 19:10:35 -0800

    added first version of wave animation to home header

--- src/components/coffee-fill/index.js
+++ src/components/coffee-fill/index.js
@@ -4,8 +4,10 @@ import './coffee-fill.scss';
 export default (props) => {
 
   return (
-     <div className="coffee-fill-container" style={{height: '78%'}}>
-
+     <div className="coffee-fill-container" style={{height: '98%'}}>
+      <div className="wave-1"></div>
+      <div className="wave-2"></div>
+      <div className="wave-3"></div>
      </div>
   );
 }
