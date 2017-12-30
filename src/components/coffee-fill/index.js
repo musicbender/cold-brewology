6067e501fb873e3a7ad3ba898eaa67c6278ec1a2
musicbender <musicbender@gmail.com>
2017-12-29 20:26:00 -0800

    initial styling of home cta section

--- src/components/coffee-fill/index.js
+++ src/components/coffee-fill/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import './coffee-fill.scss';
 
 export default (props) => {
-
   return (
-     <div className="coffee-fill-container" style={{height: '98%'}}>
+     <div className="coffee-fill-container" style={{height: '91%'}}>
       <div className="wave-1"></div>
       <div className="wave-2"></div>
       <div className="wave-3"></div>
--- src/components/home-cta-bar/index.js
+++ src/components/home-cta-bar/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import './home-cta-bar.scss';
 
 export default (props) => {
+  const topBar = (
+    <div className="cta-bar-top">
+      <div className="cell-1"></div>
+      <div className="cell-2"></div>
+      <div className="cell-3"></div>
+    </div>
+  );
+
+  const middle = (
+    <div className="cta-middle">
+      <div className="cell-1"></div>
+      <div className="cell-2"></div>
+    </div>
+  );
+
+  const bottomBar = (
+    <div className="cta-bar-bottom">
+
+    </div>
+  );
+
   return (
-     <div>
-       
-     </div>
+     <section className="home-cta-section">
+       { topBar }
+       { middle }
+       { bottomBar }
+     </section>
   );
 }
